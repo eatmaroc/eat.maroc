ec99e931d0763e4a30d406b7e15fd82925a34798
dakir <dakir@gmail.com>
2024-04-15 15:37:55 +0000

    addpromo finished

--- app/src/main/java/com/example/eatma/AddPromo.java
+++ app/src/main/java/com/example/eatma/AddPromo.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -12,16 +14,22 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
 
 public class AddPromo extends AppCompatActivity {
     EditText title, type, prix, whatsapp, description, adress;
@@ -29,12 +37,14 @@ public class AddPromo extends AppCompatActivity {
     Spinner spinnerVille, spinnerQuartier;
             Button Ajouter;
             ImageButton addpic;
-            Uri imgUri;
-            String imgUrl ="";
             StorageReference storageReference;
+
+        ArrayList<String> cityList,quartierList;
+
 
             boolean isImageChosen = false; // Flag to indicate whether an image is chosen
             Uri chosenImageUri; // Store the chosen image URI
+        Promo promo=new Promo();
 
 @SuppressLint({"MissingInflatedId", "WrongViewCast"})
 @Override
@@ -42,14 +52,6 @@ protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_add_promo);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-        Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-        v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-        return insets;
-        });
-
-        storageReference = FirebaseStorage.getInstance().getReference();
-        databaseReference = FirebaseDatabase.getInstance().getReference().child("villes");
         title = findViewById(R.id.titreInpute);
         type = findViewById(R.id.typeInput);
         prix = findViewById(R.id.prixInput);
@@ -60,6 +62,34 @@ protected void onCreate(Bundle savedInstanceState) {
         spinnerQuartier = findViewById(R.id.spinner2);
         Ajouter = findViewById(R.id.btnAjouterProme);
         addpic = findViewById(R.id.imgInput);
+        cityList=new ArrayList<>();
+        quartierList=new ArrayList<>();
+
+
+        if (spinnerVille != null) {
+                spinnerVille.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                                String selectedVille = cityList.get(position);
+                                fetchQuartiers(selectedVille);
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+                                // Logique en cas de sélection vide
+                        }
+                });
+        } else {
+                Toast.makeText(this, "Erreur lors de la récupération du Spinner Ville", Toast.LENGTH_SHORT).show();
+        }
+
+
+        storageReference = FirebaseStorage.getInstance().getReference();
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("villes");
+
+        fetchVilles();
+
+
 
 
 
@@ -75,18 +105,17 @@ public void onClick(View v) {
 @Override
 public void onClick(View v) {
         if (isImageChosen) {
-        uploadImage(chosenImageUri);
-                Promo promo=new Promo();
-                promo.setTitle("pizza");
-                promo.setType("type");
-                promo.setPrix("prix");
-                promo.setWhatsapp("numero");
-                promo.setDescription("description");
-                promo.setAdress("adresse");
-                promo.setVille("Marrakech");
-                promo.setQuartier("Mhamid");
-                promo.setImage(imgUrl);
-                addpromo(promo);
+
+                promo.setTitle(title.getText().toString());
+                promo.setType(type.getText().toString());
+                promo.setPrix(prix.getText().toString());
+                promo.setWhatsapp(whatsapp.getText().toString());
+                promo.setDescription(description.getText().toString());
+                promo.setAdress(adress.getText().toString());
+                promo.setVille(spinnerVille.getSelectedItem().toString());
+                promo.setQuartier(spinnerQuartier.getSelectedItem().toString());
+                uploadImage(chosenImageUri);
+
         } else {
         Toast.makeText(AddPromo.this, "Veuillez sélectionner une image", Toast.LENGTH_SHORT).show();
         }
@@ -119,7 +148,8 @@ private void uploadImage(Uri imageUri) {
         Toast.makeText(AddPromo.this, "Image uploaded successfully", Toast.LENGTH_SHORT).show();
         // You can get the URL of the uploaded image if needed
         fileReference.getDownloadUrl().addOnSuccessListener(uri -> {
-        imgUrl = uri.toString();
+        promo.setImage(uri.toString());
+        addpromo(promo);
 
         });
         })
@@ -146,6 +176,67 @@ private void addpromo(Promo promo) {
                                 Toast.makeText(AddPromo.this, "Failed to add promo: " + e.getMessage(), Toast.LENGTH_SHORT).show();
                         });
         }
+
+
+        private void fetchVilles() {
+                databaseReference.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                cityList.clear();
+                                for (DataSnapshot snap : snapshot.getChildren()) {
+                                        String villeName = snap.getKey();
+                                        cityList.add(villeName);
+                                }
+                                updateSpinnerAdapter();
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+                                Toast.makeText(AddPromo.this, "Failed to fetch villes: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                });
+        }
+
+        private void fetchQuartiers(String selectedVille) {
+                DatabaseReference villeRef = databaseReference.child(selectedVille).child("quartiers");
+                villeRef.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                quartierList.clear();
+                                for (DataSnapshot quartierSnapshot : snapshot.getChildren()) {
+                                        String quartierName = quartierSnapshot.getKey();
+                                        quartierList.add(quartierName);
+                                }
+                                updateQuartierSpinnerAdapter(); // Rafraîchir l'adaptateur du Spinner Quartier
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+                                Toast.makeText(AddPromo.this, "Failed to fetch quartiers: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                });
+        }
+
+
+
+        private void updateSpinnerAdapter() {
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, cityList);
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinnerVille.setAdapter(adapter);
+
+                // Appel pour mettre à jour le spinner du quartier après avoir mis à jour le spinner de la ville
+                updateQuartierSpinnerAdapter();
+        }
+
+
+        private void updateQuartierSpinnerAdapter() {
+                ArrayAdapter<String> quartierAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, quartierList);
+                quartierAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinnerQuartier.setAdapter(quartierAdapter);
+        }
+
+
+
 
 
 }
