d4b1cb42784e8bc1d5baf2cb165c47d1e7df9ae9
Dakir <dakirabdellah3@gmail.com>
2024-04-15 12:58:53 +0100

    add promo(Promo promo) methode added

--- app/src/main/java/com/example/eatma/AddPromo.java
+++ app/src/main/java/com/example/eatma/AddPromo.java
@@ -1,6 +1,8 @@
 package com.example.eatma;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class AddPromo extends AppCompatActivity {
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,20 @@ public class AddPromo extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("villes");
+    }
+
+    private void addpromo(Promo promo) {
+        String selectedVille = promo.getVille(); // Hardcoded for testing, you should get this dynamically
+        String selectedQuartier = promo.getQuartier(); // Hardcoded for testing, you should get this dynamically
+        DatabaseReference villeRef = databaseReference.child(selectedVille).child("quartiers").child(selectedQuartier);
+        String promoKey =String.format("Promo:%s",promo.getTitle());
+        villeRef.child(promoKey).setValue(promo)
+                .addOnSuccessListener(aVoid -> {
+                    Toast.makeText(AddPromo.this, "Promo added successfully!", Toast.LENGTH_SHORT).show();
+                })
+                .addOnFailureListener(e -> {
+                    Toast.makeText(AddPromo.this, "Failed to add promo: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                });
     }
 }
