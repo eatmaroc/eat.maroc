5513da26c44e81f1fc7bcc3b62ab433c293830b8
manourMed <medmanour@gmail.com>
2024-04-18 12:24:57 +0000

    style added

--- app/src/main/java/com/example/eatma/adminHome.java
+++ app/src/main/java/com/example/eatma/adminHome.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,7 +15,8 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class adminHome extends AppCompatActivity {
-Button logout,addVille,addPromo,afficherPromo;
+Button logout,addVille,addPromo,afficherPromo,addAnddelettype;
+ImageView imageView;
 FirebaseAuth auth = FirebaseAuth.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +25,21 @@ FirebaseAuth auth = FirebaseAuth.getInstance();
         addVille = findViewById(R.id.btnAjouterVille);
         addPromo=findViewById(R.id.btnAjouterPromo);
         afficherPromo=findViewById(R.id.btnAfficherPromo);
+        imageView=findViewById(R.id.close);
+        addAnddelettype=findViewById(R.id.AjouterAndSuprimmertype);
         logout=findViewById(R.id.logout);
         setButtonClickListener(addVille, AddVilleAndCartier.class);
         setButtonClickListener(addPromo, AddPromo.class);
         setButtonClickListener(afficherPromo, AfficherPromo.class);
+        setButtonClickListener(addAnddelettype, AjouterSupprimierType.class);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
         auth.addAuthStateListener(new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
